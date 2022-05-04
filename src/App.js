11449f3bc2b942dffe9fbc1c7b14be73356d06ec
manish <manishwild1000@gmail.com>
2022-05-04 10:06:44 +0200

    auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify'
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED - Amazon Cognito Region
+    region: 'eu-center-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'eu-central-1_ayJ1jkTKC',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '6otmejo8vqjihka2n0q26lturk',
+
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <Authenticator hideSignUp={true}>
+      {({ signOut, user }) => (
+              <main>
+                <h1>Hello {user.username}</h1>
+                <button onClick={signOut}>Sign out</button>
+              </main>
+            )}
+    </Authenticator>
+
+  )
 }
 
-export default App;
+export default App
